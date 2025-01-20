d0bd67581f2b1ea615f75acdbc438e7073b14143
SKishchenko <userg@yandex.ru>
2025-01-20 19:58:45 +0300

    Исправление

--- credit-application-service/src/main/java/com/example/creditapp/event/CreditDecisionEvent.java
+++ credit-application-service/src/main/java/com/example/creditapp/event/CreditDecisionEvent.java
@@ -3,8 +3,10 @@ package com.example.creditapp.event;
 import com.example.creditapp.model.CreditStatus;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class CreditDecisionEvent {
+public class CreditDecisionEvent implements Serializable {
     private Long applicationId;
     private boolean approved;
     private CreditStatus status;
